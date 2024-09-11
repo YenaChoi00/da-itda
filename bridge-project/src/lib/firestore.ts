61a23649183c76fd7da47da61cc3c107dda6a844
sehandev <sehandev@gmail.com>
2024-09-11 16:00:31 +0900

    EDIT firestore: load secrets from json file

--- bridge-project/src/lib/firestore.ts
+++ bridge-project/src/lib/firestore.ts
@@ -1,13 +1,37 @@
-import { initializeApp } from 'firebase/app';
-import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { collection, Firestore, getDocs, getFirestore } from 'firebase/firestore';
+import { existsSync, readFileSync } from 'fs';
+import { join } from 'path';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {};
+let app: FirebaseApp | undefined;
+let db: Firestore | undefined;
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+try {
+  const configPath = join(process.cwd(), 'firebase.json');
 
+  if (!existsSync(configPath)) {
+    throw new Error('firebase.json does not exist');
+  }
+
+  // Load Firebase configuration from firebase.json
+  const firebaseConfig = JSON.parse(readFileSync(configPath, 'utf8'));
+
+  app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+} catch (error) {
+  console.error(
+    'Error initializing Firebase:',
+    error instanceof Error ? error.message : String(error),
+  );
+}
+
+// FIXME: remove test code
 export async function getTest() {
+  if (!db) {
+    console.error('Firebase is not initialized. Cannot perform getTest()');
+    return;
+  }
+
   const querySnapshot = await getDocs(collection(db, 'user-dev'));
   querySnapshot.forEach((doc) => {
     console.log(`${doc.id} => `, doc.data());
