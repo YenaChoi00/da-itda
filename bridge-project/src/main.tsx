27d44ea31701e57584f1ac4491f454fa384bfa08
Choi Yena <choiyn33@naver.com>
2024-09-11 16:07:22 +0900

    Feat: Connect Firestore with Test File

--- bridge-project/src/main.tsx
+++ bridge-project/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import FamPage from './page/FamPage/FamPage.tsx';
+import TestComponent from './test-component.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <FamPage />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <FamPage />
+  // </React.StrictMode>,
+  <TestComponent></TestComponent>,
 );
